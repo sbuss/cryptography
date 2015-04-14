770e6011b879dbc1979895446eceef70326a1491
Steven Buss <steven.buss@gmail.com>
2015-04-14 13:22:52 -0400

    Switch _GCMSizevalidator to a more generic _SizeValidator

--- src/cryptography/hazmat/backends/utils.py
+++ src/cryptography/hazmat/backends/utils.py
@@ -5,34 +5,15 @@
 from __future__ import absolute_import, division, print_function
 
 
-class _GCMSizeValidator(object):
-    """
-    GCM may only encrypt up to 2**39 - 256 bits of plaintext, so we
-    must track the number of bytes we see.
-    """
-    _PLAINTEXT_BIT_LIMIT = 2 ** 39 - 256
-    _AAD_BIT_LIMIT = 2 ** 64 - 1
+class _SizeValidator(object):
+    def __init__(self, max_bits, label):
+        self._max_bits = max_bits
+        self._len = 0
+        self._label = label
 
-    def __init__(self):
-        self._plaintext_len = 0
+    def update(self, data):
+        self._len += len(data) * 8
 
-    def update_and_validate_plaintext(self, data):
-        self._plaintext_len += len(data) * 8
-        self.validate_plaintext_len()
-
-    def validate_plaintext_len(self):
-        # Technically a size of 0 is invalid, but there are a couple hacks
-        # around some things that call update(b"") on a newly initialized
-        # CipherContext, and we can't interfere with that.
-        # Also size 0 plaintext shouldn't produce any output so I don't
-        # think we need to worry about it.
-        if (self._plaintext_len < 0 or
-                self._plaintext_len > self._PLAINTEXT_BIT_LIMIT):
-            raise ValueError("Exceeded GCM mode plaintext bit limit.")
-
-    def validate_aad(self, data):
-        return self.validate_aad_len(len(data) * 8)
-
-    def validate_aad_len(self, datalen):
-        if datalen < 0 or datalen > self._AAD_BIT_LIMIT:
-            raise ValueError("Exceeded GCM mode AAD bit limit.")
+    def validate(self):
+        if self._len > self._max_bits:
+            raise ValueError("Exceeded %s bit limit." % self._label)
--- tests/hazmat/backends/test_utils.py
+++ tests/hazmat/backends/test_utils.py
@@ -6,55 +6,36 @@ from __future__ import absolute_import, division, print_function
 
 import pytest
 
-from cryptography.hazmat.backends.utils import _GCMSizeValidator
+from cryptography.hazmat.backends.utils import _SizeValidator
 
 
-class TestGcmSizeValidator(object):
-    def test_update_and_validate_valid(self):
-        gcm_size_validator = _GCMSizeValidator()
-        assert gcm_size_validator.update_and_validate_plaintext(b"") is None
-        assert gcm_size_validator.update_and_validate_plaintext(b"0") is None
-        gcm_size_validator._plaintext_len = (
-            _GCMSizeValidator._PLAINTEXT_BIT_LIMIT)
-        assert gcm_size_validator.update_and_validate_plaintext(b"") is None
+class TestSizeValidator(object):
+    def test_update(self):
+        validator = _SizeValidator(1024, "test")
+        assert validator.update(b"1234") is None
+        assert validator._len == 4 * 8
+        assert validator.update(b"1234") is None
+        assert validator._len == (4 + 4) * 8
 
-    def test_update_and_validate_invalid(self):
-        gcm_size_validator = _GCMSizeValidator()
-        gcm_size_validator._plaintext_len = (
-            _GCMSizeValidator._PLAINTEXT_BIT_LIMIT)
+    def test_update_invalid(self):
+        validator = _SizeValidator(1024, "test")
+        assert validator.update(b"0" * 128) is None
+        assert validator.validate() is None
+        with pytest.raises(ValueError) as e:
+            validator.update(b"1")
+            validator.validate()
+        assert "test" in "%s" % e.value
+
+    def test_validate(self):
+        validator = _SizeValidator(1024, "test")
+        assert validator.validate() is None
+        validator._len = 1023
+        assert validator.validate() is None
+        validator._len = 1024
+        assert validator.validate() is None
+
+    def test_validate_invalid(self):
+        validator = _SizeValidator(1024, "test")
+        validator._len = 1025
         with pytest.raises(ValueError):
-            gcm_size_validator.update_and_validate_plaintext(b"0")
-
-    def test_validate_plaintext_len_valid(self):
-        gcm_size_validator = _GCMSizeValidator()
-        assert gcm_size_validator.validate_plaintext_len() is None
-        gcm_size_validator._plaintext_len = 1024
-        assert gcm_size_validator.validate_plaintext_len() is None
-        gcm_size_validator._plaintext_len = (
-            _GCMSizeValidator._PLAINTEXT_BIT_LIMIT)
-        assert gcm_size_validator.validate_plaintext_len() is None
-
-    def test_validate_plaintext_len_invalid(self):
-        gcm_size_validator = _GCMSizeValidator()
-        gcm_size_validator._plaintext_len = (
-            _GCMSizeValidator._PLAINTEXT_BIT_LIMIT + 1)
-        with pytest.raises(ValueError):
-            gcm_size_validator.validate_plaintext_len()
-        gcm_size_validator._plaintext_len = -1
-        with pytest.raises(ValueError):
-            gcm_size_validator.validate_plaintext_len()
-
-    def test_validate_aad_len_valid(self):
-        gcm_size_validator = _GCMSizeValidator()
-        assert gcm_size_validator.validate_aad_len(0) is None
-        assert gcm_size_validator.validate_aad_len(1024) is None
-        assert gcm_size_validator.validate_aad_len(
-            _GCMSizeValidator._AAD_BIT_LIMIT) is None
-
-    def test_validate_aad_len_invalid(self):
-        gcm_size_validator = _GCMSizeValidator()
-        with pytest.raises(ValueError):
-            gcm_size_validator.validate_aad_len(
-                _GCMSizeValidator._AAD_BIT_LIMIT + 1)
-        with pytest.raises(ValueError):
-            gcm_size_validator.validate_aad_len(-1)
+            validator.validate()
